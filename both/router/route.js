bfb62f71c4f45b6ad42f01edac4a46d396aaf381
bukhtawar <bukhtawarelahi@gmail.com>
2016-02-24 14:12:40 +0500

    Routing and Layout

--- both/router/route.js
+++ both/router/route.js
@@ -1,12 +1,41 @@
 /**
- * Created by root on 2/23/16.
+ * Created by Bukhtawar Elahi on 2/23/16.
  */
-FlowRouter.route('/',{
+FlowRouter.route(['/','/home'],{
     action:function() {
+        console.log("running '/' action to rnder layouts");
         FlowLayout.render('layout', {sidebar: 'sidebar', main: 'home', cart: 'cart'})
     }
 });
+FlowRouter.route('/register',{
+    action:function() {
+        console.log("running 'register' action to render layouts");
+        FlowLayout.render('layout', {sidebar: '', main: 'register', cart: ''})    }
+});
+FlowRouter.route('/signin',{
+    action:function() {
+        console.log("running 'signin' action to rnder layouts");
+        FlowLayout.render('layout', {sidebar: '', main: 'signin', cart: ''})
+    }
+});
+FlowRouter.route('/checkout',{
+    action:function() {
+        console.log("running action checkout to rnder layouts");
+        FlowLayout.render('layout', {sidebar: '', main: 'checkout', cart: ''})
+    }
+});
 FlowRouter.route('/category/:categoryName', {
+    subscriptions:function(params){
+        console.log("SUBSCRIBE",params);
+        //make sure subsc exists
+    },
+    triggersEnter:function(params){
+        console.log("ENTER",params);
+        //check user is logged in or not
+    },
+    triggersExit:function(params){
+        console.log("EXIT",params);
+    },
     action:function (){
         FlowLayout.render('layout',{sidebar:'sidebar',main:'category',cart:'cart'});
     }
